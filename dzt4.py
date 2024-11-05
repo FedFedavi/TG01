e0969734e39c9527feebfd3baa55a9a41dcaafe4
FedFedavi <kykypyk@gmail.com>
2024-11-05 20:43:02 +0400

    dz4 file & code

---
+++ dzt4.py
@@ -0,0 +1,66 @@
+import asyncio
+import random
+
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message, FSInputFile, CallbackQuery
+
+from gtts import gTTS
+import os
+
+from config import TOKEN
+from googletrans import Translator
+import keyboards as kb
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+translator = Translator()
+
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer('_____', reply_markup=kb.main)
+
+
+@dp.callback_query(F.data == 'view')
+async def view(callback: CallbackQuery):
+    await callback.message.edit_text('_____', reply_markup=kb.inline_keyboard_op)
+
+
+@dp.callback_query(F.data == 'Опция 1')
+async def op1(callback: CallbackQuery):
+    await callback.message.answer('Это текст при нажатии инлайн кнопки Опция 1')
+
+
+@dp.callback_query(F.data == 'Опция 2')
+async def op2(callback: CallbackQuery):
+    await callback.message.answer('Это текст при нажатии инлайн кнопки Опция 2')
+
+
+@dp.message(Command('dynamic'))
+async def dynamic(message: Message):
+    await message.answer('_____', reply_markup=kb.inline_keyboard_dynamic)
+
+
+@dp.message(Command('links'))
+async def links(message: Message):
+    await message.answer('_____', reply_markup=kb.inline_keyboard_test)
+
+
+@dp.message(F.text == "Привет")
+async def test_button1(message: Message):
+    name = message.from_user.first_name
+    await message.answer(f'Привет! {name}')
+
+
+@dp.message(F.text == "Пока")
+async def test_button2(message: Message):
+    name = message.from_user.first_name
+    await message.answer(f'До свиданья, {name}')
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
--- keyboards.py
+++ keyboards.py
@@ -2,16 +2,26 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMar
 from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 
 main = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text='Тестовая кнопка 1')],
-    [KeyboardButton(text='Тестовая кнопка 2'), KeyboardButton(text='Тестовая кнопка 3')]
+    [KeyboardButton(text='Привет'), KeyboardButton(text='Пока')]
 ], resize_keyboard=True)
 
 
 inline_keyboard_test = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text='Каталог', callback_data='catalog')],
-    [InlineKeyboardButton(text='Новости', callback_data='news')],
-    [InlineKeyboardButton(text='Профиль', callback_data='person')]
+    [InlineKeyboardButton(text='Новости', url='https://newdaynews.ru/allnews/')],
+    [InlineKeyboardButton(text='Музыка', url='https://zvuk.com/playlist/1062105/')],
+    [InlineKeyboardButton(text='Видео', url='https://video-preview.s3.yandex.net/OjEsUgIAAAA.mp4')]
 ])
+
+inline_keyboard_op = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Опция 1', callback_data='Опция 1')],
+    [InlineKeyboardButton(text='Опция 2', callback_data='Опция 2')]
+])
+
+inline_keyboard_dynamic = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Показать больше', callback_data='view')]
+])
+
+
 
 test = ['Кнопка 1', 'Кнопка 2', 'Кнопка 3', 'Кнопка 4']
 
